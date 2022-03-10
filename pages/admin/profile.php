c38f26a5690ceb753fe8f4f563aa5239a7a423ee
Vrushank796 <42446203+Vrushank796@users.noreply.github.com>
2022-03-09 19:23:45 -0500

    Added similar products

--- pages/admin/profile.php
+++ pages/admin/profile.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Admin Panel-Admin profile</title>
+	
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="styles.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -37,7 +38,7 @@
 								<button class="dropbtn"><i class="fas fa-user"></i></button>
 								<div class="dropdown-content">
 									<a href="profile.php">My profile</a>
-									<a href="logout_admin.php">Logout</a>
+									<a href="../signin.php">Logout</a>
 								</div>
 							</div>
 						</div>
